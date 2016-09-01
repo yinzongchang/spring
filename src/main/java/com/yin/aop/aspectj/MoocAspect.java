cd5e3b8c13b9168d06401b0280a67c112caca504
yinzongchang <yinzongchang@meicai.cn>
2016-09-01 21:48:49 +0800

    AOP

--- src/main/java/com/yin/aop/aspectj/MoocAspect.java
+++ src/main/java/com/yin/aop/aspectj/MoocAspect.java
@@ -1,5 +1,10 @@
 package com.yin.aop.aspectj;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -18,9 +23,44 @@ public class MoocAspect {
 		
 	}
 	
-	@Before("execution(* com.yin.aop.aspectj.biz.*Biz.*(..))")
+//	@Before("execution(* com.yin.aop.aspectj.biz.*Biz.*(..))")
+	@Before("pointcut()")
 	public void before() {
 		System.out.println("Before...");
+	}
+	
+	@Before("pointcut() && args(arg)")
+	public void beforeWithParam(String arg) {
+		System.out.println("Before: " + arg);
+	}
+	
+	@Before("pointcut() && @annotation(moocMethod)")
+	public void beforeWithAnnotaion(MoocMethod moocMethod) {
+		System.out.println("BeforeWithAnnotain.: " + moocMethod.value());
+	}
+	
+	@AfterReturning(pointcut="bizPointcut()", returning="returnVal")
+	public void afterReturning(Object returnVal) {
+		System.out.println("AfterReturning: " + returnVal);
+	}
+	
+	@AfterThrowing(pointcut="pointcut()", throwing="e")
+	public void afterThrowing(RuntimeException e) {
+		System.out.println("AfterThrow: " + e.getMessage());
+	}
+	
+	@After("pointcut()")
+	public void after() {
+		System.out.println("After...");
+	}
+	
+	@Around("pointcut()")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("Around 1 .");
+		Object obj = pjp.proceed();
+		System.out.println("Around 2 .");
+		System.out.println("Around: " + obj);
+		return obj;
 	}
 
 }
--- src/main/java/com/yin/aop/aspectj/biz/MoocBiz.java
+++ src/main/java/com/yin/aop/aspectj/biz/MoocBiz.java
@@ -2,11 +2,15 @@ package com.yin.aop.aspectj.biz;
 
 import org.springframework.stereotype.Service;
 
+import com.yin.aop.aspectj.MoocMethod;
+
 @Service
 public class MoocBiz {
 	
+	@MoocMethod("MoocBiz save MoocMethod")
 	public String save(String arg) {
 		System.out.println("MoocBiz save: " + arg);
+//		throw new RuntimeException("Savefailed!");
 		return "Save success!!!";
 	}
 
